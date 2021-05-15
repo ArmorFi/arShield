bad1d624ef79c81059698c1d2b65f162f8395c37
RobertMCForster <rforster510@yahoo.com>
2021-05-15 15:49:00 -0700

    Update arShield.sol

--- contracts/core/arShield.sol
+++ contracts/core/arShield.sol
@@ -1,65 +1,47 @@
-pragma solidity ^0.6.6;
-
+pragma solidity 0.6.12;
 import '../general/SafeMath.sol';
-import '../general/ArmorModule.sol';
 import '../interfaces/IyDAI.sol';
-import '../interfaces/IArmorMaster.sol';
-import '../interfaces/IPlanManager.sol';
-import '../interfaces/IClaimManager.sol';
-import '../interfaces/IBalanceManager.sol';
-import '../client/ArmorClient.sol';
 
 /**
  * @title Armor Shield Yearn
  * @dev arShield accepts Yearn tokens, returns arTokens, those tokens are automatically insured.  
  * @author Armor.Fi -- Robert M.C. Forster
 **/
-contract ArShieldYearn is ArmorClient {
+contract ArShieldYearn {
 
     using SafeMath for uint;
 
     // Buffer amount for division.
     uint256 constant private BUFFER = 1e18;
-    
     // The protocol that this contract buys coverage for (Nexus Mutual address for Uniswap/Balancer/etc.).
     address[] public protocols;
-    
     // If the Yearn vault is a Curve token, we must unwrap before selling.
     bool public crvToken;
-    
-    uint256 public vaultValue;
-    
+    // Ether value of this shield.
+    uint256 public ethValue;
     // Beneficiary may withdraw any extra Ether after a claims period.
     address payable public beneficiary;
-    
     // Whether or not the contract is locked.
     bool public locked;
-    
     // Block at which users must be holding tokens to receive a payout.
     uint256 public payoutBlock;
-    
+    // Amount of Ether given to user who notified of a hack.
     uint256 public depositReward;
+    // Amount of Ether required to notify of a hack and temporarily lock contract.
     uint256 public depositAmount;
+    // User who deposited to notify of a hack.
     address public depositor;
-    
     // Delay (in seconds) between mint request and actual minting.
     uint256 public mintDelay;
-    
     // 0.25% paid for minting in order to pay for the first week of coverage--can be immediately liquidated.
     uint256 public mintFees;
-    
-    // Last time an EOA has called this contract.
-    mapping (address => uint256) public lastCall;
-    
     // User => timestamp of when minting was requested.
     mapping (address => MintRequest) public mintRequests;
-
 
     // Protocol token that we're providing protection for.
     IyDAI public pToken;
     // Underlying token of the pToken.
     IERC20 public uToken;
-
 
     // Time and amount a user would like to mint.
     struct MintRequest {
@@ -67,10 +49,9 @@ contract ArShieldYearn is ArmorClient {
         uint208 requestAmount;
     }
 
-
-    // event mint requestAmount
-    // event mint finalize
-    // event redeem
+    event MintRequest(address user, uint256 amount, uint256 timestamp);
+    event MintFinalized(address user, uint256 amount, uint256 timestamp);
+    event Redemption(address user, uint256 amount, uint256 timestamp);
 
     modifier locked {
         require(locked, "You may not do this while the contract is unlocked.");
@@ -82,22 +63,19 @@ contract ArShieldYearn is ArmorClient {
         require(!locked, "You may not do this while the contract is locked.");
         _;
     }
-
-    // Avoid composability issues for liquidation.
-    modifier notContract {
-        require(msg.sender == tx.origin, "Sender must be an EOA.");
-        _;
-    }
-    
-    // Must be able to receive Ether from the arCore claim manager.
-    receive() external payable { }
     
     /**
      * @dev Initialize the contract
      * @param _master Address of the Armor master contract.
      * @param _pToken The protocol token we're protecting.
     **/
-    function initialize(address _pToken, address _uToken, address _uniPool, address _master, bool _crvToken)
+    function initialize(
+        address _pToken, 
+        address _uToken, 
+        address _uniPool, 
+        address _master, 
+        bool _crvToken
+    )
       external
     {
         IArmorMaster(_master).initialize();
@@ -111,61 +89,41 @@ contract ArShieldYearn is ArmorClient {
      *      Amount returned will likely be more than deposited because pTokens will be removed to pay for cover.
      * @param _pAmount Amount of pTokens to deposit to the contract.
     **/
-    function mint(uint256 _pAmount, address _beneficiary)
+    function mint(
+        uint256 _pAmount, 
+        address _beneficiary
+    )
       external
       notLocked
     {
         MintRequest memory mintRequest = mintRequests[_beneficiary];
+        
         if (block.timestamp.sub(mintDelay) > mintRequest.requestTime) {
+            
             delete mintRequests[_beneficiary];
             _mint(_beneficiary, mintRequest.requestAmount);
+            emit MintFinalized(_beneficiary, mintRequest.requestAmount, block.timestamp);
+        
         } else if (mintRequest.requestTime == 0) {
+            
             uint256 arAmount = arValue(_pAmount);
             pToken.transferFrom(_beneficiary, address(this), _pAmount);
             mintRequests[_beneficiary] = MintRequest(block.timestamp, arAmount);
+            emit MintRequest(_beneficiary, arAmount, block.timestamp);
+            
         }
     }
 
-    function redeem(uint256 _arAmount)
+    function redeem(
+        uint256 _arAmount
+    )
       external
     {
         uint256 pAmount = pValue(_arAmount);
         _burn(msg.sender, _arAmount);
         arToken.transfer(msg.sender, pAmount);
         if (locked() && address(this).balance > 0) _sendClaim(_pAmount);
-    }
-    
-    /**
-     * @dev Exchange pToken for the underlying token, sell on a dex, purchase insurance.
-     *      We're not very concerned with price manipulation on dexes because there's little
-     *      to no incentive for malicious actors and flash loans cannot be used.
-    **/
-    function liquidate()
-      external
-      notLocked
-      notContract
-    {
-        // Figure out how much value we have on contract
-        // Tell CoverageBase how much value we have and ask how much we need to pay
-        // Liquidate the amount we need to pay
-        // Deposit into coverageBase
-
-        _liquidate(addition);
-        ArmorCore.deposit(addition);
-
-        msg.sender.transfer(topUpReward);
-    }
-    
-    /**
-     * @dev Here we determine needed value of pToken, redeem tokens for value, exchange for Ether.
-     * @param _refillNeeded Amount of Ether we need to liquidate tokens for.
-    **/
-    function _liquidate(uint256 _refillNeeded)
-      internal
-    {
-    
-        uint256 ethPerToken = _findEthPerToken;
-        
+        emit Redemption(msg.sender, _arAmount, block.timestamp);
     }
 
     /**
@@ -174,7 +132,9 @@ contract ArShieldYearn is ArmorClient {
     **/
     function _findEthPerToken()
       internal
-    returns (uint256 ethPerToken)
+    returns (
+        uint256 ethPerToken
+    )
     {
         uint256 uTokenPerPToken = pToken.getPricePerFullShare();
         ethPerToken = uniswap.tokenToEther(uTokenPerPToken);       
@@ -200,7 +160,7 @@ contract ArShieldYearn is ArmorClient {
       external
       notLocked
     {
-        beneficiary.transfer(address(this).balance);
+        beneficiary.transfer( address(this).balance );
     }
 
     /**
@@ -223,10 +183,14 @@ contract ArShieldYearn is ArmorClient {
      * @param _pAmount Amount of yTokens to find arToken value of.
      * @return arAmount Amount of arToken the input pTokens are worth.
     **/
-    function arValue(uint256 _pAmount)
+    function arValue(
+        uint256 _pAmount
+    )
       public
       view
-    returns (uint256 arAmount)
+    returns (
+        uint256 arAmount
+    )
     {
         uint256 bufferedP = pToken.balanceOf( address(this) ).mul(1e18);
         uint256 bufferedAmount = totalSupply().div(bufferedP).mul(_pAmount);
@@ -238,10 +202,14 @@ contract ArShieldYearn is ArmorClient {
      * @param _arAmount Amount of arTokens to find yToken value of.
      * @return pAmount Amount of pTokens the input arTokens are worth.
     **/
-    function pValue(uint256 _arAmount)
+    function pValue(
+        uint256 _arAmount
+    )
       public
       view
-    returns (uint256 pAmount)
+    returns (
+        uint256 pAmount
+    )
     {
         uint256 bufferedP = pToken.balanceOf( address(this) ) * 1e18;
         uint256 bufferedAmount = bufferedP.div( totalSupply() ).mul(_arAmount);
@@ -251,7 +219,9 @@ contract ArShieldYearn is ArmorClient {
     /**
      * @dev Sends Ether if the contract is locked and Ether is in it.
     **/
-    function _sendEther(uint256 _arAmount)
+    function _sendEther(
+        uint256 _arAmount
+    )
       internal
     {
         uint256 ethAmount = _arAmount * BUFFER / totalSupply() * address(this).balance / BUFFER;
@@ -261,7 +231,9 @@ contract ArShieldYearn is ArmorClient {
     /**
      * @dev Used by controller to confirm that a hack happened, which then locks the contract in anticipation of claims.
     **/
-    function confirmHack(uint256 _payoutBlock)
+    function confirmHack(
+        uint256 _payoutBlock
+    )
       external
       onlyController
     {
@@ -284,7 +256,9 @@ contract ArShieldYearn is ArmorClient {
     /**
      * @dev Controller can change different delay periods on the contract.
     **/
-    function changeDelays(uint256 _mintDelay)
+    function changeDelays(
+        uint256 _mintDelay
+    )
       external
       onlyController
     {
