080e2851bf00417fed414f39b09f415ee4fe6ae5
Robert M.C. Forster <robert@armor.fi>
2021-05-24 23:44:24 -0700

    Adding some half-finished

--- contracts/core/Liquidation.sol
+++ contracts/core/Liquidation.sol
@@ -1,24 +1,59 @@
 pragma solidity 0.6.12;
 
+import './IYearn.sol';
+
 
 contract YearnLiquidation is Liquidation {
+
+    uint256 constant DENOMINATOR = 1000;
+    // Bonus received by liquidators. 5 == 0.5% off Chainlink price.
+    uint256 public bonus;
+    uint256 public lock;
+    
+    modifier nonReentrant {
+        uint256 start = lock;
+        lock++;
+        _;
+        require(lock == start + 1, "Re-entrancy protection.");
+    }
     
     function liquidate()
       external
       override
+      nonReentrant
     {
-        // this will get the amount owed in Ether
-        uint256 owed = super.getOwed();
-        uint256 toRedeem = ethToUnderlying(owed);
-        uint256 underlyingOwed = redeemYearn(owed);
-        liquidate(owed);
+        // Find amount owed in Ether, find amount owed in underlying ('u'), redeem Yearn for that amount, send to liquidator, get Ether back.
+        uint256 ethOwed = getOwed();
+        require(msg.value >= ethOwed, "Must send correct Ether amount.");
+        
+        uint256 baseRedeem = ethToU(ethOwed);
+        // Add a bonus of 0.5%.
+        uint256 bonusRedeem = baseRedeem * bonus / DENOMINATOR;
+        uint256 yToRedeem = uToY(bonusRedeem);
+        redeemYearn(yToRedeem);
+        uToken.safeTransfer(msg.sender, bonusRedeem);
+        
+        uint256 ethValue = yToken.balanceOf( address(this) ) * ethOwed / yToRedeem; 
+        covBase.updateShield(ethValue);
     }
     
-    function redeemYearn()
-      external
-      override
+    function uToY(
+        uint256 _uShares
+    )
+      public
+      view
+    returns(
+        uint256 yShares
+    )
     {
-        // call Yearn to liquidate token into underlying
+        uint256 oneYToken = yVault.calc(1 ether);
+        yShares = _uShares * 1 ether / oneYToken;
+    }
+    
+    function redeemYearn(uint256 _shares)
+      internal
+    {
+        yVault.withdraw(_shares, address(this), 1);
     }
     
     function ethToUnderlying(
@@ -26,24 +61,14 @@ contract YearnLiquidation is Liquidation {
     )
       public
       view
+    returns(
+        uint256 uOwed
+    )
     {
-        // find eth value of the underlying tokens
+        uint256 ethPrice = chainlink.ethPrice;
+        uint256 uPrice = chainlink.uPrice;
+        
     }
-    
-    // liquidate:
-        // ask coverageBase how much we need to pay 
-        // get price with super.chainlink
-        // sell on super.uniswap
-        // coverageBase.deposit()
-        
-    // purchase
-        // ask coverageBase how much we need to pay
-        // get price with super.chainlink
-        // msg.value to return correct tokens
-        // coverageBase.deposit()
-        
-    // redeem
-        // if super.
     
     /**
      * @dev Finds the amount of cover required to protect all holdings and returns Ether value of 1 token.
@@ -55,8 +80,19 @@ contract YearnLiquidation is Liquidation {
         uint256 ethPerToken
     )
     {
-        uint256 uTokenPerPToken = pToken.getPricePerFullShare();
-        ethPerToken = uniswap.tokenToEther(uTokenPerPToken);       
+        // change to chainlink
+    }
+    
+    /**
+     * @dev Edit the discount on Chainlink price that liquidators receive.
+    **/
+    function editBonus(
+        uint256 _newBonus
+    )
+      external
+      onlyOwner
+    {
+        bonus = _newBonus;
     }
     
 }
